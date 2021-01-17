1912b8040c99f9bc02f42bd07123ffdd16b66fa6
unknown <jbo.7991@gmail.com>
2021-01-17 00:57:52 -0500

    third commit

--- Model/Credentials.php
+++ Model/Credentials.php
@@ -1,8 +1,8 @@
 <?php
 //Login data for the database. Use this file in all Models
-$host = "coffee.cs18nbncvmqr.us-east-1.rds.amazonaws.com";
+$host = "10.0.2.79";
 $user = "coffee";
 $passwd = "12345678-Aa";
-$database = "coffeedb";
+$database = "coffee";
 
 ?>
